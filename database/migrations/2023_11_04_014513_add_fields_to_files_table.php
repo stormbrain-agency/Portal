ea9c5afaaa45d2d805d8f348d7e96181d89d4cc0
Storm Brain <dev@stormbrain.us>
2023-11-07 08:41:38 +0700

    update db

---
+++ database/migrations/2023_11_04_014513_add_fields_to_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->string('uploaded_by', 50);
+            $table->string('comment', 150); 
+            $table->timestamps();
+        });
+
+        Schema::table('files', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->dropTimestamps(); 
+        });
+    }
+};
