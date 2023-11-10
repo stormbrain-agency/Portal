9510e79e552dbf72e0e311f7f82007e3f353fb82
stormbrain <dev@stormbrain.us>
2023-11-10 16:28:41 +0700

    update fix w9 download

--- resources/views/pages/apps/user-management/users-pending/columns/_w9-file.blade.php
+++ resources/views/pages/apps/user-management/users-pending/columns/_w9-file.blade.php
@@ -1,3 +1,3 @@
 @if ($user->w9Upload && count($user->w9Upload) > 0 )
-   <a href="{{ route('county-provider-w9.w9_download', ['filename' => $user->w9Upload->first()?->original_name]) }}" class="btn btn-primary bnt-active-light-primary btn-sm">Download</a>
+   <a href="{{ route('w9_upload.w9_download', ['filename' => $user->w9Upload->first()?->original_name]) }}" class="btn btn-primary bnt-active-light-primary btn-sm">Download</a>
 @endif
