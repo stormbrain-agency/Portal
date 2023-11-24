a1a6ebffbd698d3cf2fec8c0c11547be7cf0f0ec
Storm Brain <73203051+stormbrain-agency@users.noreply.github.com>
2023-11-24 09:59:10 +0700

    fix noti error

--- resources/views/pages/apps/provider-w9/upload.blade.php
+++ resources/views/pages/apps/provider-w9/upload.blade.php
@@ -29,11 +29,13 @@
         <div class="card-body py-4">
             <!-- <span class="text-gray-700 fs-6"><i>If you have any questions, refer to our FAQs or submit a request via our contact us form.</i> </span> -->
             <br>
+            
+
             <form action="/county-w9/upload" method="POST" id="myform" class="form" enctype="multipart/form-data">
                 @csrf
-                 <input type="hidden" name="csrf_token" value="{{ csrf_token() }}">
                 <!--begin::Scroll-->
                 <div class="d-flex flex-column">
+
                     <!--begin::Input group-->
                     <div class="fv-row mb-7">
                         <!--begin::Label-->
@@ -50,7 +52,7 @@
                                 <!--begin::Info-->
                                 <div class="ms-4">
                                     <h3 class="fs-5 fw-bold text-gray-900 mb-1 mt-3">Upload ZIP File of provider Payment Report Submission (files).</h3>
-                                    <p class="fs-7 fw-semibold text-gray-500">Drag & Drop or choose files from computer</p>
+                                    <p class="fs-7 fw-semibold text-gray-500 drop-zone__prompt">Drag & Drop or choose files from computer</p>
                                     <p class="fs-7 fw-semibold text-gray-500 font-italic">
                                         <i>
                                             This portal site is not a storage system, but rather a secure site for transferring documents.
@@ -63,6 +65,11 @@
 
                         </div>
                         <!--end::Dropzone-->
+                       
+                        @if(session('error'))
+                            <span class="text-danger">{{ session('error') }}</span>
+                        @endif
+
                     </div>
                     <!--end::Input group-->
                     <!--begin::Input group-->
@@ -71,7 +78,7 @@
                             <label class="fw-bold fs-6 mb-2">Your Comment (max 150 characters): </label>
                             <!--end::Label-->
                             <!--begin::Input-->
-                            <textarea id="comment" name="comments" placeholder="Please write a comment here" class="border border-gray-500 mb-3 mb-lg-0 form-control bg-transparent" id="" cols="30" rows="5s"></textarea>
+                            <textarea id="comment" placeholder="Please write a comment here" class="border border-gray-500 mb-3 mb-lg-0 form-control bg-transparent" name="comments" id="" cols="30" rows="5s"></textarea>
                             <!--end::Input-->
                             @error('comment')
                             <span class="text-danger">{{ $message }}</span> @enderror
@@ -97,7 +104,7 @@
     <!-- Add this script after including the Dropzone.js library -->
     <script>
         document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-      const dropZoneElement = inputElement.closest(".drop-zone");
+      const dropZoneElement = inputElement.closest(".dropzone");
 
       dropZoneElement.addEventListener("click", (e) => {
         inputElement.click();
@@ -157,18 +164,7 @@
       }
 
       thumbnailElement.dataset.label = inputElement.files.length;
-
-      // Show thumbnail for image files
-    //   if (file.type.startsWith("image/")) {
-    //     const reader = new FileReader();
-
-    //     reader.readAsDataURL(file);
-    //     reader.onload = () => {
-    //       thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-    //     };
-    //   } else {
         thumbnailElement.style.backgroundImage = null;
-      // }
     }
 
     </script>
