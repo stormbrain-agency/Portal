60dd10b067a6603226f68dea3ec2b99b36a67b99
stormbrain <dev@stormbrain.us>
2023-11-27 20:13:30 +0700

    update county user 2

--- resources/views/pages/apps/user-management/users-county/columns/_w9-file.blade.php
+++ resources/views/pages/apps/user-management/users-county/columns/_w9-file.blade.php
@@ -1,3 +1,3 @@
 @if ($user->w9Upload && count($user->w9Upload) > 0 )
-   <a href="{{ route('w9_upload.w9_download', ['w9_id' => $user->w9Upload->first()?->original_name,'filename' => $user->w9Upload->first()?->original_name]) }}" class="btn btn-primary bnt-active-light-primary btn-sm">Download</a>
+   <a href="{{ route('w9_upload.w9_download', ['w9_id' => $user->w9Upload->first()?->id,'filename' => $user->w9Upload->first()?->original_name]) }}" class="btn btn-primary bnt-active-light-primary btn-sm">Download</a>
 @endif
