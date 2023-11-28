a7f024a14d6dddbfc402729411e481e219dc1812
dev@stormbrain.us <dev@stormbrain.us>
2023-11-28 14:45:40 +0700

    update interface

--- resources/views/pages/apps/provider-w9/upload.blade.php
+++ resources/views/pages/apps/provider-w9/upload.blade.php
@@ -65,13 +65,15 @@
                         <!--end::Dropzone-->
                        
                         @if(session('error'))
-                            <div class="wrap-alert error d-flex align-items-center mt-6">
-                              {!! getIcon('notification-bing','me-4') !!}
-                              <div class="content" style="width: 100%;">
-                                  <div class="title mb-2">There is Error Uploading Your File</div>
-                                  <div class="sub-title">{{ session('error') }}</div>
-                              </div>
-                              {!! getIcon('cross','fs-1 btn-alert') !!}
+                          <div class="alert bg-light-danger border-danger d-flex align-items-center p-5 mt-5">
+                            <i class="ki-duotone ki-shield-tick fs-2hx text-danger me-4"><span class="path1"></span><span class="path2"></span></i>
+                            <div class="d-flex flex-column">
+                                <h4 class="mb-1 text-dark">This is an alert</h4>
+                                <span>The alert component can be used to highlight certain parts of your page for higher content visibility.</span>
+                            </div>
+                            <button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto" data-bs-dismiss="alert">
+                                <i class="ki-duotone ki-cross fs-1 text-danger"><span class="path1"></span><span class="path2"></span></i>
+                            </button>
                           </div> 
                         @endif
 
