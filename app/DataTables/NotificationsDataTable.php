b10e8949d90fd299977f2bc70092aeb10108b3f0
“Stombrain” <dev@stombrain.us>
2023-11-30 00:42:00 +0700

    update notificaitons

--- app/DataTables/NotificationsDataTable.php
+++ app/DataTables/NotificationsDataTable.php
@@ -77,7 +77,38 @@ class NotificationsDataTable extends DataTable
             ->addTableClass('table align-middle table-row-dashed fs-6 gy-5 dataTable no-footer text-gray-600 fw-semibold')
             ->setTableHeadClass('text-start text-muted fw-bold fs-7 text-uppercase gs-0')
             ->orderBy([0, 'asc'])
-            ->drawCallback("function() {" . file_get_contents(resource_path('views/pages/apps/notifications/columns/_draw-scripts.js')) . "}");
+            ->drawCallback("function() {" . file_get_contents(resource_path('views/pages/apps/notifications/columns/_draw-scripts.js')) . "}")
+            ->drawCallback("function() {
+                    $('.status-toggle').on('click', function () {
+                        var statusCheckbox = $(this);
+                        var status = statusCheckbox.prop('checked') ? 'Active' : 'Unactive';
+                        var notificationId = statusCheckbox.data('notification-id');
+
+                        console.log('Notification ID:', notificationId);
+                        console.log('Status:', status);
+
+                        $.ajax({
+                            url: '" . route("notification-management.update-status") . "',
+                            method: 'POST',
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name=\"csrf-token\"]').attr('content')
+                            },
+                            data: {
+                                id: notificationId,
+                                status: status
+                            },
+                            success: function (response) {
+                                console.log(response);
+                                alert('Changes');
+                            },
+                            error: function (error) {
+                                console.log(error);
+                                alert('No Changes');
+                            }
+                        });
+                    });
+                }
+            ");
     }
 
     /**
--- resources/views/livewire/notifications/notification-status.blade.php
+++ resources/views/livewire/notifications/notification-status.blade.php
@@ -5,7 +5,40 @@
             data-kt-action="view_rows"
             class="form-check-input status-toggle"
             type="checkbox"
+            data-notification-id="{{ $notification->id }}"
             {{ $notification->status === 'Active' ? 'checked' : '' }}
         >
     </div>
 </div>
+{{-- <script>
+    $(document).ready(function () {
+        $('.status-toggle').on('click', function () {
+            var statusCheckbox = $(this);
+            var status = statusCheckbox.prop('checked') ? 'Active' : 'Unactive';
+            var notificationId = statusCheckbox.data('notification-id');
+
+            console.log('Notification ID:', notificationId);
+            console.log('Status:', status);
+
+            $.ajax({
+                url: '{{ route("notification-management.update-status") }}',
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: {
+                    id: notificationId,
+                    status: status
+                },
+                success: function (response) {
+                    console.log(response);
+                    alert('Changes');
+                },
+                error: function (error) {
+                    console.log(error);
+                    alert('No Changes');
+                }
+            });
+        });
+    });
+</script> --}}
