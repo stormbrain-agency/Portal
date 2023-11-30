6d5fbfd3e7d3d33bc1add400852bbe0b094106f9
dev@stormbrain.us <dev@stormbrain.us>
2023-11-30 15:47:17 +0700

    update link css interface

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -40,7 +40,7 @@
 
     @livewireStyles
 
-    <link rel="stylesheet" href="{{ asset('assets/css/style.interface.css') }}">
+    <link rel="stylesheet" href="{{ asset('libs/css/style.interface.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
 </head>
 <!--end::Head-->
