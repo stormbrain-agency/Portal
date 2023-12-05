4c7d64dcbab28fd9f221a8df266fa9ac9d6db7a5
dev@stormbrain.us <dev@stormbrain.us>
2023-12-05 23:05:49 +0700

    update logo

--- resources/_keenthemes/src/js/components/drawer.js
+++ resources/_keenthemes/src/js/components/drawer.js
@@ -1,15 +1,15 @@
 "use strict";
 
-var KTDrawerHandlersInitialized = false; 
+var KTDrawerHandlersInitialized = false;
 
 // Class definition
-var KTDrawer = function(element, options) {
+var KTDrawer = function (element, options) {
     //////////////////////////////
     // ** Private variables  ** //
     //////////////////////////////
     var the = this;
 
-    if ( typeof element === "undefined" || element === null ) {
+    if (typeof element === "undefined" || element === null) {
         return;
     }
 
@@ -25,15 +25,15 @@ var KTDrawer = function(element, options) {
     // ** Private methods  ** //
     ////////////////////////////
 
-    var _construct = function() {
-        if ( KTUtil.data(element).has('drawer') ) {
+    var _construct = function () {
+        if (KTUtil.data(element).has('drawer')) {
             the = KTUtil.data(element).get('drawer');
         } else {
             _init();
         }
     }
 
-    var _init = function() {
+    var _init = function () {
         // Variables
         the.options = KTUtil.deepExtend({}, defaultOptions, options);
         the.uid = KTUtil.getUniqueId('drawer');
@@ -57,12 +57,12 @@ var KTDrawer = function(element, options) {
         KTUtil.data(the.element).set('drawer', the);
     }
 
-    var _handlers = function() {
+    var _handlers = function () {
         var togglers = _getOption('toggle');
         var closers = _getOption('close');
 
-        if ( togglers !== null && togglers.length > 0 ) {
-            KTUtil.on(document.body, togglers, 'click', function(e) {
+        if (togglers !== null && togglers.length > 0) {
+            KTUtil.on(document.body, togglers, 'click', function (e) {
                 e.preventDefault();
 
                 the.toggleElement = this;
@@ -70,8 +70,8 @@ var KTDrawer = function(element, options) {
             });
         }
 
-        if ( closers !== null && closers.length > 0 ) {
-            KTUtil.on(document.body, closers, 'click', function(e) {
+        if (closers !== null && closers.length > 0) {
+            KTUtil.on(document.body, closers, 'click', function (e) {
                 e.preventDefault();
 
                 the.closeElement = this;
@@ -80,12 +80,12 @@ var KTDrawer = function(element, options) {
         }
     }
 
-    var _toggle = function() {
-        if ( KTEventHandler.trigger(the.element, 'kt.drawer.toggle', the) === false ) {
+    var _toggle = function () {
+        if (KTEventHandler.trigger(the.element, 'kt.drawer.toggle', the) === false) {
             return;
         }
 
-        if ( the.shown === true ) {
+        if (the.shown === true) {
             _hide();
         } else {
             _show();
@@ -94,8 +94,8 @@ var KTDrawer = function(element, options) {
         KTEventHandler.trigger(the.element, 'kt.drawer.toggled', the);
     }
 
-    var _hide = function() {
-        if ( KTEventHandler.trigger(the.element, 'kt.drawer.hide', the) === false ) {
+    var _hide = function () {
+        if (KTEventHandler.trigger(the.element, 'kt.drawer.hide', the) === false) {
             return;
         }
 
@@ -108,15 +108,15 @@ var KTDrawer = function(element, options) {
 
         KTUtil.removeClass(the.element, the.options.baseClass + '-on');
 
-        if ( the.toggleElement !== null ) {
+        if (the.toggleElement !== null) {
             KTUtil.removeClass(the.toggleElement, 'active');
         }
 
         KTEventHandler.trigger(the.element, 'kt.drawer.after.hidden', the) === false
     }
 
-    var _show = function() {
-        if ( KTEventHandler.trigger(the.element, 'kt.drawer.show', the) === false ) {
+    var _show = function () {
+        if (KTEventHandler.trigger(the.element, 'kt.drawer.show', the) === false) {
             return;
         }
 
@@ -128,14 +128,14 @@ var KTDrawer = function(element, options) {
 
         KTUtil.addClass(the.element, the.options.baseClass + '-on');
 
-        if ( the.toggleElement !== null ) {
+        if (the.toggleElement !== null) {
             KTUtil.addClass(the.toggleElement, 'active');
         }
 
         KTEventHandler.trigger(the.element, 'kt.drawer.shown', the);
     }
 
-    var _update = function() {
+    var _update = function () {
         var width = _getWidth();
         var direction = _getOption('direction');
 
@@ -145,17 +145,17 @@ var KTDrawer = function(element, options) {
         var end = _getOption('end');
 
         // Reset state
-        if ( KTUtil.hasClass(the.element, the.options.baseClass + '-on') === true && String(document.body.getAttribute('data-kt-drawer-' + the.name + '-')) === 'on' ) {
+        if (KTUtil.hasClass(the.element, the.options.baseClass + '-on') === true && String(document.body.getAttribute('data-kt-drawer-' + the.name + '-')) === 'on') {
             the.shown = true;
         } else {
             the.shown = false;
-        }       
+        }
 
         // Activate/deactivate
-        if ( _getOption('activate') === true ) {
+        if (_getOption('activate') === true) {
             KTUtil.addClass(the.element, the.options.baseClass);
             KTUtil.addClass(the.element, the.options.baseClass + '-' + direction);
-            
+
             KTUtil.css(the.element, 'width', width, true);
             the.lastWidth = width;
 
@@ -216,8 +216,8 @@ var KTDrawer = function(element, options) {
         }
     }
 
-    var _createOverlay = function() {
-        if ( _getOption('overlay') === true ) {
+    var _createOverlay = function () {
+        if (_getOption('overlay') === true) {
             the.overlayElement = document.createElement('DIV');
 
             KTUtil.css(the.overlayElement, 'z-index', KTUtil.css(the.element, 'z-index') - 1); // update
@@ -226,30 +226,30 @@ var KTDrawer = function(element, options) {
 
             KTUtil.addClass(the.overlayElement, _getOption('overlay-class'));
 
-            KTUtil.addEvent(the.overlayElement, 'click', function(e) {
+            KTUtil.addEvent(the.overlayElement, 'click', function (e) {
                 e.preventDefault();
 
-                if ( _getOption('permanent') !== true ) {
+                if (_getOption('permanent') !== true) {
                     _hide();
                 }
             });
         }
     }
 
-    var _deleteOverlay = function() {
-        if ( the.overlayElement !== null ) {
+    var _deleteOverlay = function () {
+        if (the.overlayElement !== null) {
             KTUtil.remove(the.overlayElement);
         }
     }
 
-    var _getOption = function(name) {
-        if ( the.element.hasAttribute('data-kt-drawer-' + name) === true ) {
+    var _getOption = function (name) {
+        if (the.element.hasAttribute('data-kt-drawer-' + name) === true) {
             var attr = the.element.getAttribute('data-kt-drawer-' + name);
             var value = KTUtil.getResponsiveValue(attr);
 
-            if ( value !== null && String(value) === 'true' ) {
+            if (value !== null && String(value) === 'true') {
                 value = true;
-            } else if ( value !== null && String(value) === 'false' ) {
+            } else if (value !== null && String(value) === 'false') {
                 value = false;
             }
 
@@ -257,7 +257,7 @@ var KTDrawer = function(element, options) {
         } else {
             var optionName = KTUtil.snakeToCamel(name);
 
-            if ( the.options[optionName] ) {
+            if (the.options[optionName]) {
                 return KTUtil.getResponsiveValue(the.options[optionName]);
             } else {
                 return null;
@@ -265,17 +265,17 @@ var KTDrawer = function(element, options) {
         }
     }
 
-    var _getWidth = function() {
+    var _getWidth = function () {
         var width = _getOption('width');
 
-        if ( width === 'auto') {
+        if (width === 'auto') {
             width = KTUtil.css(the.element, 'width');
         }
 
         return width;
     }
 
-    var _destroy = function() {
+    var _destroy = function () {
         KTUtil.data(the.element).remove('drawer');
     }
 
@@ -287,54 +287,54 @@ var KTDrawer = function(element, options) {
     ///////////////////////
 
     // Plugin API
-    the.toggle = function() {
+    the.toggle = function () {
         return _toggle();
     }
 
-    the.show = function() {
+    the.show = function () {
         return _show();
     }
 
-    the.hide = function() {
+    the.hide = function () {
         return _hide();
     }
 
-    the.isShown = function() {
+    the.isShown = function () {
         return the.shown;
     }
 
-    the.update = function() {
+    the.update = function () {
         _update();
     }
 
-    the.goElement = function() {
+    the.goElement = function () {
         return the.element;
     }
 
-    the.destroy = function() {
+    the.destroy = function () {
         return _destroy();
     }
 
     // Event API
-    the.on = function(name, handler) {
+    the.on = function (name, handler) {
         return KTEventHandler.on(the.element, name, handler);
     }
 
-    the.one = function(name, handler) {
+    the.one = function (name, handler) {
         return KTEventHandler.one(the.element, name, handler);
     }
 
-    the.off = function(name, handlerId) {
+    the.off = function (name, handlerId) {
         return KTEventHandler.off(the.element, name, handlerId);
     }
 
-    the.trigger = function(name, event) {
+    the.trigger = function (name, event) {
         return KTEventHandler.trigger(the.element, name, event, the, event);
     }
 };
 
 // Static methods
-KTDrawer.getInstance = function(element) {
+KTDrawer.getInstance = function (element) {
     if (element !== null && KTUtil.data(element).has('drawer')) {
         return KTUtil.data(element).get('drawer');
     } else {
@@ -343,7 +343,7 @@ KTDrawer.getInstance = function(element) {
 }
 
 // Hide all drawers and skip one if provided
-KTDrawer.hideAll = function(skip = null, selector = '[data-kt-drawer="true"]') {
+KTDrawer.hideAll = function (skip = null, selector = '[data-kt-drawer="true"]') {
     var items = document.querySelectorAll(selector);
 
     if (items && items.length > 0) {
@@ -355,8 +355,8 @@ KTDrawer.hideAll = function(skip = null, selector = '[data-kt-drawer="true"]') {
                 continue;
             }
 
-            if ( skip ) {
-                if ( item !== skip ) {
+            if (skip) {
+                if (item !== skip) {
                     drawer.hide();
                 }
             } else {
@@ -367,7 +367,7 @@ KTDrawer.hideAll = function(skip = null, selector = '[data-kt-drawer="true"]') {
 }
 
 // Update all drawers
-KTDrawer.updateAll = function(selector = '[data-kt-drawer="true"]') {
+KTDrawer.updateAll = function (selector = '[data-kt-drawer="true"]') {
     var items = document.querySelectorAll(selector);
 
     if (items && items.length > 0) {
@@ -382,11 +382,11 @@ KTDrawer.updateAll = function(selector = '[data-kt-drawer="true"]') {
 }
 
 // Create instances
-KTDrawer.createInstances = function(selector = '[data-kt-drawer="true"]') {
+KTDrawer.createInstances = function (selector = '[data-kt-drawer="true"]') {
     // Initialize Menus
     var elements = document.querySelectorAll(selector);
 
-    if ( elements && elements.length > 0 ) {
+    if (elements && elements.length > 0) {
         for (var i = 0, len = elements.length; i < len; i++) {
             new KTDrawer(elements[i]);
         }
@@ -394,22 +394,22 @@ KTDrawer.createInstances = function(selector = '[data-kt-drawer="true"]') {
 }
 
 // Toggle instances
-KTDrawer.handleShow = function() {
+KTDrawer.handleShow = function () {
     // External drawer toggle handler
-    KTUtil.on(document.body,  '[data-kt-drawer-show="true"][data-kt-drawer-target]', 'click', function(e) {
+    KTUtil.on(document.body, '[data-kt-drawer-show="true"][data-kt-drawer-target]', 'click', function (e) {
         e.preventDefault();
-        
+
         var element = document.querySelector(this.getAttribute('data-kt-drawer-target'));
 
         if (element) {
             KTDrawer.getInstance(element).show();
-        } 
+        }
     });
 }
 
 // Handle escape key press
-KTDrawer.handleEscapeKey = function() {
-    document.addEventListener('keydown', (event) => {        
+KTDrawer.handleEscapeKey = function () {
+    document.addEventListener('keydown', (event) => {
         if (event.key === 'Escape') {
             //if esc key was not pressed in combination with ctrl or alt or shift
             const isNotCombinedKey = !(event.ctrlKey || event.altKey || event.shiftKey);
@@ -417,23 +417,23 @@ KTDrawer.handleEscapeKey = function() {
                 var elements = document.querySelectorAll('.drawer-on[data-kt-drawer="true"]:not([data-kt-drawer-escape="false"])');
                 var drawer;
 
-                if ( elements && elements.length > 0 ) {
+                if (elements && elements.length > 0) {
                     for (var i = 0, len = elements.length; i < len; i++) {
                         drawer = KTDrawer.getInstance(elements[i]);
                         if (drawer.isShown()) {
                             drawer.hide();
                         }
                     }
-                }              
+                }
             }
         }
     });
 }
 
 // Dismiss instances
-KTDrawer.handleDismiss = function() {
+KTDrawer.handleDismiss = function () {
     // External drawer toggle handler
-    KTUtil.on(document.body,  '[data-kt-drawer-dismiss="true"]', 'click', function(e) {
+    KTUtil.on(document.body, '[data-kt-drawer-dismiss="true"]', 'click', function (e) {
         var element = this.closest('[data-kt-drawer="true"]');
 
         if (element) {
@@ -441,21 +441,21 @@ KTDrawer.handleDismiss = function() {
             if (drawer.isShown()) {
                 drawer.hide();
             }
-        } 
+        }
     });
 }
 
 // Handle resize
-KTDrawer.handleResize = function() {
+KTDrawer.handleResize = function () {
     // Window resize Handling
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         var timer;
 
-        KTUtil.throttle(timer, function() {
+        KTUtil.throttle(timer, function () {
             // Locate and update drawer instances on window resize
             var elements = document.querySelectorAll('[data-kt-drawer="true"]');
 
-            if ( elements && elements.length > 0 ) {
+            if (elements && elements.length > 0) {
                 for (var i = 0, len = elements.length; i < len; i++) {
                     var drawer = KTDrawer.getInstance(elements[i]);
                     if (drawer) {
@@ -468,7 +468,7 @@ KTDrawer.handleResize = function() {
 }
 
 // Global initialization
-KTDrawer.init = function() {
+KTDrawer.init = function () {
     KTDrawer.createInstances();
 
     if (KTDrawerHandlersInitialized === false) {
