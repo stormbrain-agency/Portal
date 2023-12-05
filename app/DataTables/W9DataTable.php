211fba63cb5b2851760c2b0c7371a206b6e05578
Storm Brain <73203051+stormbrain-agency@users.noreply.github.com>
2023-12-05 16:21:49 +0700

    update code  w9

--- app/DataTables/W9DataTable.php
+++ app/DataTables/W9DataTable.php
@@ -72,11 +72,12 @@ class W9DataTable extends DataTable
         $query->join('users', 'w9_upload.user_id', '=', 'users.id')
               ->join('counties', 'w9_upload.w9_county_fips', '=', 'counties.county_fips') 
               ->where('users.status', 1)
-              ->select('w9_upload.*', 'counties.*','w9_upload.id','w9_upload.created_at'); 
+              ->select('w9_upload.*','w9_upload.created_at', 'counties.county','w9_upload.id'); 
         
         if (auth()->user()->hasRole('county user')) {
             $query->where('users.id', auth()->user()->id);
         }
+        
         return $query;
     }
 
@@ -113,8 +114,8 @@ class W9DataTable extends DataTable
         if (auth()->user()->hasRole('view only')) {
             return [
                 Column::make('id')->name("w9_upload.id")->title('ID'),
-                Column::make('created_at')->name("w9_upload.created_at")->title('Date'),
-                Column::make('updated_at')->title('Time')->name('w9_upload.created_at')->orderable(true),
+                Column::make('created_at')->name("w9_upload.created_at")->title('Date')->orderable(true)->searchable(true),
+                Column::make('updated_at')->title('Time')->name('w9_upload.created_at')->orderable(true)->searchable(true),
                 Column::make('w9_county_fips')->title('Country Designation')->name('counties.county')->orderable(true)->searchable(true),
                 Column::make('user')->title('USER WHO SUBMITTED')->name('users.first_name')->orderable(true),
                 Column::make('email')->name("users.email")->visible(false),
@@ -130,7 +131,7 @@ class W9DataTable extends DataTable
         if (auth()->user()->hasRole('county user')) {
             return [
                 Column::make('id')->name("w9_upload.id")->title('ID'),
-                Column::make('created_at')->name("w9_upload.created_at")->title('Date'),
+                Column::make('created_at')->name("w9_upload.created_at")->title('Date')->orderable(true)->searchable(true),
                 Column::make('updated_at')->title('Time')->name('w9_upload.created_at')->orderable(true),
                 Column::make('w9_county_fips')->title('Country Designation')->name('counties.county')->orderable(true)->searchable(true),
                 Column::make('user')->title('USER WHO SUBMITTED')->name('users.first_name')->orderable(true),
@@ -141,7 +142,7 @@ class W9DataTable extends DataTable
         }else {
             return [
                 Column::make('id')->name("w9_upload.id")->title('ID'),
-                Column::make('created_at')->name("w9_upload.created_at")->title('Date'),
+                Column::make('created_at')->name("w9_upload.created_at")->title('Date')->orderable(true)->searchable(true),
                 Column::make('updated_at')->title('Time')->name('w9_upload.created_at')->orderable(true),
                 Column::make('w9_county_fips')->title('Country Designation')->name('counties.county')->orderable(true)->searchable(true),
                 Column::make('user')->title('USER WHO SUBMITTED')->name('users.first_name')->orderable(true),
--- resources/views/pages/apps/provider-w9/upload.blade.php
+++ resources/views/pages/apps/provider-w9/upload.blade.php
@@ -68,8 +68,8 @@
                             <div class="alert bg-light-danger border-danger d-flex align-items-center p-5 mt-5">
                                 <i class="ki-duotone ki-shield-tick fs-2hx text-danger me-4"><span class="path1"></span><span class="path2"></span></i>
                                 <div class="d-flex flex-column">
-                                    <h4 class="mb-1 text-dark">This is an alert</h4>
-                                    <span>The alert component can be used to highlight certain parts of your page for higher content visibility.</span>
+                                    <h4 class="mb-1 text-dark">Error</h4>
+                                    <span>{{ session('error') }}</span>
                                 </div>
                                 <button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto" data-bs-dismiss="alert">
                                     <i class="ki-duotone ki-cross fs-1 text-danger"><span class="path1"></span><span class="path2"></span></i>
