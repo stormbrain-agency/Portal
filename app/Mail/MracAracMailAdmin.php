e22fa0a2ec5dca113587eee71887d8eca8cac204
“Stombrain” <dev@stombrain.us>
2023-12-06 13:53:17 +0700

    update noti email

--- app/Mail/MracAracMailAdmin.php
+++ app/Mail/MracAracMailAdmin.php
@@ -8,6 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use App\Models\NotificationMail;
 
 class MracAracMailAdmin extends Mailable
 {
@@ -32,15 +33,44 @@ class MracAracMailAdmin extends Mailable
     }
 
     /**
+     * Get email content from the database based on the name_form.
+     *
+     * @return array
+     */
+    protected function getEmailContent(): array
+    {
+        $nameForm = $this->data['name_form'] ?? null;
+
+        if ($nameForm) {
+            $notificationMail = NotificationMail::where('name_form', $nameForm)->first();
+
+            if ($notificationMail) {
+                return [
+                    'subject' => $notificationMail->subject,
+                    'body' => $notificationMail->body,
+                    'button_title' => $notificationMail->button_title,
+                ];
+            }
+        }
+        // Default values if no matching record is found
+        return [
+            'subject' => 'Default Subject',
+            'body' => 'Default Body',
+            'button_title' => 'Default Button Title',
+        ];
+    }
+
+    /**
      * Get the message content definition.
      */
     public function content(): Content
     {
-        
+        $emailContent = $this->getEmailContent();
         return new Content(
             view: 'mail.admin.mrac-arac',
             with: [
                 'data' => $this->data,
+                'emailContent' => $emailContent,
             ]
         );
     }
