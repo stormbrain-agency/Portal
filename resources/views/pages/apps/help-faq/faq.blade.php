3a7e724824017002d90882f58b5e5d1f32ef4572
dev@stormbrain.us <dev@stormbrain.us>
2023-12-07 10:03:01 +0700

    update faq

--- resources/views/pages/apps/help-faq/faq.blade.php
+++ resources/views/pages/apps/help-faq/faq.blade.php
@@ -17,7 +17,12 @@
                 <h3 class="title-question ">
                     How does CDA know which Stage One or Bridge Program providers to pay?            
                 </h3>
-                <div class="content ">However, in order for CDA to issue payment, eligible child care providers will receive an email requesting to submit a valid W-9 Form.</div>
+                <div class="content ">
+                    CDSS has provided CDA with the information for those providers eligible to be paid the one-time payment. These are providers whose local County Human Services Department is unable to provide those payments. Counties include:
+                    <br>
+                    <br>
+                    Colusa, San Diego, Yolo, El Dorado, Imperial, Madera, Mendocino, Monterey, Riverside, San Bernardino, Stanislaus, Sutter, Tehama, Glenn, Mono, Sierra, Contra Costa, Fresno, Orange, San Mateo, Santa Barbara, Santa Clara, Santa Cruz, Sonoma, Sacramento, and Tulare.
+                </div>
             </div>
         </div>
         <div class="box-question bg-state-body d-flex align-items-start" onclick="toggleAnswer(this)">
@@ -26,7 +31,9 @@
                 <h3 class="title-question ">
                     How will child care providers receive the one-time Transitional Subsidy payments?           
                 </h3>
-                <div class="content ">However, in order for CDA to issue payment, eligible child care providers will receive an email requesting to submit a valid W-9 Form.</div>
+                <div class="content ">
+                    CDA will issue a check for the one-time payment directly to eligible Stage One or Bridge Program child care providers whose valid W-9 Form has been received.
+                </div>
             </div>
         </div>
         <div class="box-question bg-state-body d-flex align-items-start" onclick="toggleAnswer(this)">
@@ -34,7 +41,7 @@
             <div class="wrap-content">
                 <h3 class="title-question ">
                     When will child care providers receive the one-time Transitional Provider Subsidy payments?                </h3>
-                <div class="content ">However, in order for CDA to issue payment, eligible child care providers will receive an email requesting to submit a valid W-9 Form.</div>
+                <div class="content ">CDA will issue payments no later than <strong>November 30, 2023</strong> to eligible providers whose valid W-9 Form has been received.</div>
             </div>
         </div>
     </div>
